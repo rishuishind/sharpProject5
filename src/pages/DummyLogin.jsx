98bbb457e7f34d8bd8ee6b0a49c173117da931a9
rishabh <hespnod2348@gmai.com>
2023-12-12 18:15:58 +0530

    veried the user using email

--- src/pages/DummyLogin.jsx
+++ src/pages/DummyLogin.jsx
@@ -2,12 +2,42 @@ import React from 'react'
 import { Link } from 'react-router-dom/cjs/react-router-dom';
 
 const DummyLogin = () => {
+
+    const handleVerifyEmail = () => {
+        const token = localStorage.getItem('token');
+        fetch('https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyD8ycB6q6pys2MMvD6gP4F308TdRu3RshI', {
+            method: 'POST',
+            body: JSON.stringify({
+                requestType: 'VERIFY_EMAIL',
+                idToken: token
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => {
+            if (res.ok) {
+                return res.json();
+            } else {
+                return res.json().then(data => {
+                    let errorMsg = 'Authentication Failed';
+                    throw new console.error(errorMsg);
+                })
+            }
+        }).then(data => {
+            console.log(data);
+        }).catch(err => {
+            alert(err);
+        })
+    }
+
     return (
         <>
             <h1>This is a dummy login page</h1>
             <Link to='/updateProfile'>
                 <h1 className=' text-blue-800'>Your Profile is incomplete!! Complete now</h1>
             </Link>
+
+            <button onClick={handleVerifyEmail} className=' bg-red-500 text-white p-2 mt-5 rounded-lg'>Verify Email</button>
         </>
     )
 }
